5302f375a2bbab4e971102d5d0a5c7f75cfdb8c0
Javier Brea <javier.brea@gmail.com>
2018-06-09 19:58:34 +0200

    Fix auth api

---
+++ test/integration/specs/auth-api-no-admin.specs.js
@@ -0,0 +1,12 @@
+
+const test = require('narval')
+
+test.describe('auth api', () => {
+  test.describe('when requested by no-admin users', () => {
+    test.it('should return an error when trying to add a new api key', () => {
+    })
+
+    test.it('should return an error when trying to delete an existant api key', () => {
+    })
+  })
+})
