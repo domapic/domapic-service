a8ec481036fbb18bb9000a1ca8f6dc4c71104590
Javier Brea <javier.brea@gmail.com>
2018-09-23 11:41:54 +0200

    Add authorization settings to tests

---
+++ test/end-to-end/commands/get-controller-api-key.js
@@ -0,0 +1,26 @@
+
+const testUtils = require('narval/utils')
+
+const getLogs = () => {
+  return testUtils.logs.combined('controller')
+    .then(log => {
+      const match = log.match(/Use the next api key to register services: (\S*)\n/)
+      if (match && match[1]) {
+        console.log(match[1])
+        return Promise.resolve()
+      }
+      return waitAndGetLogs()
+    })
+}
+
+const waitAndGetLogs = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      getLogs().then(() => {
+        resolve()
+      })
+    }, 2000);
+  })
+}
+
+getLogs()
--- test/end-to-end/commands/local-clean.sh
+++ test/end-to-end/commands/local-clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pm2 flush
 pm2 delete controller
 
 ./test/end-to-end/commands/clean-db.sh
