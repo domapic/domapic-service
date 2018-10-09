187fa6a31c7f2e9d1ee5344ba7f6682edcee4f76
Javier Brea <javier.brea@gmail.com>
2018-10-09 20:30:59 +0200

    Add reconnection end-to-end test

--- test/end-to-end/commands/start-service.sh
+++ test/end-to-end/commands/start-service.sh
@@ -4,4 +4,4 @@ API_KEY=$(node test/end-to-end/commands/get-controller-api-key.js)
 
 echo "CONTROLLER API KEY--------------> ${API_KEY}"
 
-node test/end-to-end/fixtures/${fixture}/server.js --name=${fixture} --hostName=${service_host_name} --port=${service_port} --path=${domapic_path} --controller=http://${controller_host_name}:3000 --controllerApiKey=${API_KEY} --authDisabled
+node test/end-to-end/fixtures/${fixture}/server.js --name=${fixture} --hostName=${service_host_name} --port=${service_port} --path=${domapic_path} --controller=http://${controller_host_name}:3000 --controllerApiKey=${API_KEY} --authDisabled --logLevel=debug
