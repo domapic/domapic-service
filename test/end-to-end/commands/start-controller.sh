a95420af32d26a4f4ffb565dcd30c0d2a879a22e
Javier Brea <javier.brea@gmail.com>
2018-10-10 17:59:24 +0200

    Fixtures path pointing to shared path

--- test/end-to-end/commands/start-controller.sh
+++ test/end-to-end/commands/start-controller.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd test/end-to-end/fixtures/${fixture}
-rm -rf ${domapic_path}/controller/logs
+pm2 flush
 npm i
-npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=${domapic_path} --db=${db_uri} --save --authDisabled
+npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=../../../../${domapic_path} --db=${db_uri} --save --authDisabled
 npm run domapic-controller logs controller
