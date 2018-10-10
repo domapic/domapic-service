c7d5dbe4c6e9fb154fe30b39b1363bf53baba38b
Javier Brea <javier.brea@gmail.com>
2018-10-10 18:26:20 +0200

    Use pm2 binary to flush logs

--- test/end-to-end/commands/start-controller.sh
+++ test/end-to-end/commands/start-controller.sh
@@ -2,6 +2,6 @@
 
 cd test/end-to-end/fixtures/${fixture}
 npm i
-./node_modules/.bin/pm2 flush
+../../../../node_modules/.bin/pm2 flush
 npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=../../../../${domapic_path} --db=${db_uri} --save --authDisabled
 npm run domapic-controller logs controller
