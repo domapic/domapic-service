25974e4c44f0d250bd6fccbdf5bb486d29417eae
Javier Brea <javier.brea@gmail.com>
2018-10-12 09:44:35 +0200

    Add connection api used with wrong controller key e2e test

---
+++ test/end-to-end/specs/connection-api-wrong-key.specs.js
@@ -0,0 +1,22 @@
+
+const test = require('narval')
+
+const utils = require('./utils')
+
+test.describe('when using connection api to connect with controller with wrong controller apiKey', function () {
+  this.timeout(10000)
+  const serviceConnection = new utils.ServiceConnection()
+
+  test.it('should return an authentication error', () => {
+    return serviceConnection.request('/connection', {
+      method: 'PUT',
+      body: {
+        active: true,
+        apiKey: "foo-api-key",
+        url: utils.CONTROLLER_URL
+      }
+    }).then(response => {
+      return test.expect(response.statusCode).to.equal(401)
+    })
+  })
+})
---
+++ test/end-to-end/specs/not-connected.specs.js
@@ -0,0 +1,17 @@
+
+const test = require('narval')
+const testUtils = require('narval/utils')
+
+const utils = require('./utils')
+
+test.describe('when service finish connection process', function () {
+  this.timeout(10000)
+
+  test.it('should have not connected to the controller', () => {
+    return testUtils.logs.combined('service')
+      .then((log) => {
+        return test.expect(log).to.not.contain(`Connection success with Domapic Controller`)
+      })
+  })
+})
+
