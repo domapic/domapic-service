3a5639f78c912722e0f027fed02bd789e9a13c29
Javier Brea <javier.brea@gmail.com>
2018-11-09 19:23:33 +0100

    Upgrade base and controller. Define service type

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const domapic = require('domapic-base')
 const options = require('./lib/options')
 const ServiceHandler = require('./lib/ServiceHandler')
 
-const createModule = moduleOptions => new domapic.Service(options.extendWith(moduleOptions))
+const createModule = moduleOptions => domapic.Service(options.extendWith(moduleOptions))
   .then(service => {
     const serviceHandler = new ServiceHandler(service)
     return Promise.all([
