7614a3508eb0844b78e1d4fac7a1aeb8e1549489
Javier Brea <javier.brea@gmail.com>
2018-11-10 09:26:23 +0100

    Add custom options documentation

--- test/functional/fixtures/example/cli.js
+++ test/functional/fixtures/example/cli.js
@@ -1,7 +1,10 @@
 const path = require('path')
 const domapic = require('../../../../index')
 
+const options = require('./options')
+
 domapic.cli({
   packagePath: path.resolve(__dirname),
-  script: path.resolve(__dirname, 'server.js')
+  script: path.resolve(__dirname, 'server.js'),
+  customConfig: options
 })
