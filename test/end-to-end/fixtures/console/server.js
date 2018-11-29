2aa7a45678d88a35308b41203df8ef9a8781c097
Javier Brea <javier.brea@gmail.com>
2018-11-29 20:53:51 +0100

    Add end-to-end tests for abilities with no data

--- test/end-to-end/fixtures/console/server.js
+++ test/end-to-end/fixtures/console/server.js
@@ -9,10 +9,10 @@ domapic.createModule({
 }).then((service) => {
   let lastCharacter = ''
 
-  const consoleLog = function (data) {
+  const consoleLog = function (data, eventName) {
     lastCharacter = data
     console.log(`Printing into console: ${data}`)
-    service.events.emit('console', data)
+    service.events.emit(eventName, data)
   }
 
   return service.register({
@@ -35,7 +35,40 @@ domapic.createModule({
       action: {
         description: 'Print the received character into console',
         handler: (data) => {
-          consoleLog(data)
+          consoleLog(data, 'console')
+          return Promise.resolve(data)
+        }
+      }
+    },
+    consoleNoData: {
+      description: 'Handle console log with no data',
+      event: {
+        description: 'Console with no data received'
+      },
+      action: {
+        description: 'Print the received character into console',
+        handler: () => {
+          console.log('Received action with no data')
+          service.events.emit('consoleNoData')
+          return Promise.resolve()
+        }
+      }
+    },
+    consoleNumeric: {
+      description: 'Handle console log with numeric data',
+      data: {
+        type: 'number',
+        minimum: 1,
+        maximum: 10
+        // TODO, add an enum
+      },
+      event: {
+        description: 'Console with numeric data received'
+      },
+      action: {
+        description: 'Print the received number into console',
+        handler: (data) => {
+          consoleLog(data, 'consoleNumeric')
           return Promise.resolve(data)
         }
       }
