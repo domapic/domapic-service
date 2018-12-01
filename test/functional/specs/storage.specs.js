d63489844d2d1078f11a194649f0c11366bf262f
Javier Brea <javier.brea@gmail.com>
2018-12-01 11:27:35 +0100

    Add trace for debugging docker tests in CI

--- test/functional/specs/storage.specs.js
+++ test/functional/specs/storage.specs.js
@@ -6,6 +6,7 @@ test.describe('service storage', function () {
   test.it('should have saved test value to storage', () => {
     return utils.readStorage()
       .then(storedData => {
+        console.log(storedData)
         return test.expect(storedData.storageTest).to.equal('foo-storage-value')
       })
   })
